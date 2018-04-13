c254fcb0bf120de457229a55cb9d024cbe362676
markbookk <marcos.nieves1@upr.edu>
2018-04-13 15:03:02 -0400

    Changed platform and changed random speeds.

--- src/rbadia/voidspace/main/MegaManMain.java
+++ src/rbadia/voidspace/main/MegaManMain.java
@@ -61,7 +61,8 @@ public class MegaManMain {
 			LevelState level3State = new Level3State(3, frame, gameStatus, gameLogic, inputHandler, graphicsMan, soundMan);
 			LevelState level4State = new Level4State(4, frame, gameStatus, gameLogic, inputHandler, graphicsMan, soundMan);
 			LevelState level5State = new Level5State(5, frame, gameStatus, gameLogic, inputHandler, graphicsMan, soundMan);
-			LevelState levels[] = { level1State, level2State, level3State, level4State, level5State };
+//			LevelState levels[] = { level1State, level2State, level3State, level4State, level5State };
+			LevelState levels[] = { level3State };
 
 			String outcome = "CONGRATS!! YOU WON!!";
 			for (LevelState nextLevel : levels) {
